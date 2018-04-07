000de76376c15949216d55e3e2eabc98c32f0bf8
bafab <bafa@tutanota.de>
2018-04-07 16:06:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-const driver = {};
-
-function updateDriverWithKeyAndValue (driver, key, value) {
-  return Object.assign({}, driver, {[key]: value});
-}
-
-function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
-  driver[key] = value;
-  return driver;
-}
-
-function deleteFromDriverByKey (driver, key) {
-  const newObj = Object.assign({}, driver);
-  delete newObj[key];
-  return newObj;
-}
-
-function destructivelyDeleteFromDriverByKey (driver, key) {
-  delete driver[key];
-  return driver;
-}
+const driver = {}
+function updateDriverWithKeyAndValue(driver, key, value) { return Object.assign({}, driver, { [key]: value }); }
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) { driver[key] = value; return driver }
+function deleteFromDriverByKey(driver, key) { return delete driver.key }
+function destructivelyDeleteFromDriverByKey(driver, key) { return delete driver[key] }
